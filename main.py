6e02a2ff5db9481324d573079c0159d76c8466ab
carlyjb17 <69774917+carlyjb17@users.noreply.github.com>
2021-01-24 01:24:55 +0100

    Muchos cambios demasiados quiza

--- main.py
+++ main.py
@@ -1,32 +1,45 @@
+import threading
 from mcapi import *
 from org.bukkit.event.entity import PlayerDeathEvent
 from org.bukkit.event.player import PlayerRespawnEvent
 from org.bukkit.event.entity import EntityDamageEvent
 from org.bukkit.event.player import PlayerVelocityEvent
 
-#beta-dev0.3
+#beta-dev0.5
+vida_infinita = False
+def se_acabo():
+    global vida_infinita
+    vida_infinita = False
+    return vida_infinita
+
 def dano_mortal(e):
+    global vida_infinita
     entidad = e.getEntity()
     entidad_str = str(entidad)
-    
+    vida_infinita = False
     if entidad_str[:11] == "CraftPlayer":
         vida = float(entidad.getHealth())
         vida_str = str(vida)
-        yell(vida_str)
-        if vida <= 1.5:
-            yell("el poder de los dioses te ha salvado")
+        #yell(vida_str)
+        #if e.getDamage() > 0 and vida_infinita == True:
+            #entidad.setHealth(20.0)
+        if e.getDamage() >= vida:
+            yell("REVIVE!!!!!!!!!!!!!!!!")
             entidad.setHealth(20.0)
+            lugar = entidad.getLocation()
+            entidad.spawnParticle(Particle.SOUL, lugar, 100)
+            entidad.playSound(lugar, Sound.BLOCK_ANVIL_LAND, 0.5, 1)
+            #vida_infinita = True
+            #t = threading.Timer(60, se_acabo)
+            #t.start()
+    
+   
+
+        
         
 
-def revisar_vida(e):
-    entidad2 = e.getPlayer()
-    
-    
-    vida2 = float(entidad2.getHealth())
-    if vida2 <= 1.5:
-        yell("el poder de los dioses te ha salvado")
-        entidad2.setHealth(20.0)
+        
+
 
 
 listener = add_event_listener(EntityDamageEvent, dano_mortal)
-listener2 = add_event_listener(PlayerVelocityEvent, revisar_vida)
